210349229eb5f05c5e7a9dce9c3e2c979a0a84d7
rodtob <rodrigo.toblli@gmail.com>
2020-10-04 09:32:05 -0300

    arreglo css mobile

--- public/javascripts/cambioMenu.js
+++ public/javascripts/cambioMenu.js
@@ -1,4 +1,6 @@
 function toggleClass(){
     let menu = document.querySelector('.menu__links')
+    let campana = document.querySelector('.campana__martillo')
     menu.classList.toggle('toggleCls')
+    campana.classList.toggle('atrasCampana')
 }
