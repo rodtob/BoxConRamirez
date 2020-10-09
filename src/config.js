b3338eba38db9560e7daa1bcefe0c13b57ed697a
rodtob <rodrigo.toblli@gmail.com>
2020-10-09 13:32:28 -0300

    prueba db

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@ module.exports = {
     database:{
         username: "bb5712fe9d5c1d",
         password: "f0ed2623",
-        database: "Ramirez",
+        // database: "Ramirez",
         host: "eu-cdbr-west-03.cleardb.net"
     }
 }
